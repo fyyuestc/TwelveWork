d644ba677e0a166bb083c2d7590261fa45736995
student <student@imac-15.local>
2018-12-06 15:28:07 +0800

    Initial Commit

---
+++ TwelveWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwelveWork
+//
+//  Created by student on 2018/12/6.
+//  Copyright © 2018年 2016110305. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
