2870bc59fa3294f7609b54b6f67fc39937ea06a4
student <student@imac-15.local>
2018-12-06 16:57:31 +0800

    基本功能

---
+++ TwelveWork/ShowTableViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ShowTableViewController.swift
+//  TwelveWork
+//
+//  Created by student on 2018/12/6.
+//  Copyright © 2018年 2016110305. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ShowTableViewController: UITableViewController {
+    //加载代理及上下文
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var players : [Player]?
+    
+    fileprivate func reloadData() {
+        let fetch:NSFetchRequest<Player> = Player.fetchRequest()
+        fetch.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        //得到全部数据
+        players = try? context.fetch(fetch)
+        tableView.reloadData()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //重新加载数据
+        reloadData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        reloadData()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return players?.count ?? 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        if let player = players?[indexPath.row] {
+            cell.textLabel?.text = player.name
+            cell.detailTextLabel?.text = player.habit
+        }
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "嫖客名单"
+    }
+    
+    
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TwelveWork/ViewController.swift
+++ TwelveWork/ViewController.swift
@@ -7,9 +7,62 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var habit: UITextField!
+    @IBOutlet weak var notice: UILabel!
+    
+    //本应用的代理(强转为自己的AppDelegate)
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    //数据库中的上下文
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    @IBAction func back(_ sender: Any) {
+         dismiss(animated: true, completion: nil)
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        appDelegate.saveContext()
+    }
+    
+    @IBAction func addPlayer(_ sender: Any) {
+        let p = Player(context: context)
+        p.name = name.text
+        p.habit = habit.text
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func updatePlayer(_ sender: Any) {
+        let fetch:NSFetchRequest<Player> = Player.fetchRequest()
+        fetch.predicate = NSPredicate(format: "name=%@",name.text!)
+        let players = try? context.fetch(fetch)
+        if let p = players?.first {
+           p.habit = habit.text
+           notice.text = "更新成功"
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func deletePlayer(_ sender: Any) {
+        let fetch:NSFetchRequest<Player> = Player.fetchRequest()
+        fetch.predicate = NSPredicate(format: "name=%@",name.text!)
+        let players = try? context.fetch(fetch)
+        if let p = players?.first {
+            context.delete(p)
+            notice.text = "删除成功"
+        }
+    }
+        
+    @IBAction func queryPlayer(_ sender: Any) {
+        let fetch:NSFetchRequest<Player> = Player.fetchRequest()
+        fetch.predicate = NSPredicate(format: "name=%@",name.text!)
+        let players = try? context.fetch(fetch)
+        if let p = players?.first {
+            name.text = p.name
+            habit.text = p.habit
+            notice.text = "查询成功"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
